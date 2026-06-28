7ef74e0b1cb3b25548fc32fb492f1c1ea8e28b61
Juviotanaka <juviotanaka@gmail.com>
2026-06-29 02:34:55 +0700

    Updated Version Patch & STD Template

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ════════════════════════════════════
 // SERVICE WORKER — offline cache + auto-update
 // ════════════════════════════════════
-const CACHE_NAME = 'venterprise-v4.40';
+const CACHE_NAME = 'venterprise-v4.42';
 
 const PRECACHE = [
   './',
